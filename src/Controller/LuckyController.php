f91463f68133630141d4bdd78303d635d1572ec3
gumme <angm23@student.bth.se>
2024-05-06 14:22:11 +0200

    Fìxed routes not being accesible and started the training example in kmom02

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -6,7 +6,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class LuckyController
 {
     #[Route('/lucky/number')]
@@ -28,5 +27,5 @@ class LuckyController
         );
     }
 
-    
+
 }
